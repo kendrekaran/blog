14307435c2d546b0a92fd50861cc895b2376febb
Karan kendre <karankendreg@gmail.com>
2024-12-31 11:15:31 +0530

    Updated Backend

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
-import { verify } from "hono/jwt";
+import { decode, verify } from "hono/jwt";
 
 export const blogRouter = new Hono<{
     Bindings: {
@@ -13,26 +13,44 @@ export const blogRouter = new Hono<{
     }
 }>();
 
-blogRouter.use("/*",async (c, next) => {
-   next()
+blogRouter.use("/*", async (c, next) => {
+    const authHeader = c.req.header("authorization") || "";
+    try {
+        const user = await verify(authHeader, c.env.JWT_SECRET);
+        if (user) {
+			//@ts-ignore
+            c.set("userId", user.id);
+            await next();
+        } else {
+            c.status(401); 
+            return c.json({
+                message: "Authentication required"
+            });
+        }
+    } catch (e) {
+        c.status(401);
+        return c.json({
+            message: "Invalid authentication token"
+        });
+    }
 });
 
 blogRouter.post('/', async (c) => {
 	const body = await c.req.json();
+	const authorId = c.get("userId")
 	const prisma = new PrismaClient({
 		datasourceUrl: c.env?.DATABASE_URL	,
 	}).$extends(withAccelerate());
 
-	const post = await prisma.post.create({
+	const blog = await prisma.blog.create({
 		data: {
 			title: body.title,
 			content: body.content,
-            // @ts-ignore
-			authorId:  1
+			authorId:  Number(authorId)
 		}
 	});
 	return c.json({
-		id: post.id
+		id: blog.id
 	});
 })
 
@@ -42,7 +60,7 @@ blogRouter.put('/', async (c) => {
 		datasourceUrl: c.env?.DATABASE_URL	,
 	}).$extends(withAccelerate());
 
-	const post = prisma.post.update({
+	const blog = await prisma.blog.update({
 		where: {
 			id: body.id
 		},
@@ -52,25 +70,36 @@ blogRouter.put('/', async (c) => {
 		}
 	});
 	return c.json({
-        id : post.id
+        id : blog.id
     })
 });
 
-blogRouter.get('/', async (c) => {
-    const body = await c.req.json()
+blogRouter.get("/bulk", async (c)=>{
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+	const blogs = await prisma.blog.findMany()
+
+	return c.json({
+		blogs
+	})
+})
+
+blogRouter.get('/:id', async (c) => {
+    const id = await c.req.param("id")
 	const prisma = new PrismaClient({
 		datasourceUrl: c.env?.DATABASE_URL	,
 	}).$extends(withAccelerate());
 	
 	try{
-        const post = await prisma.post.findFirst({
+        const blog = await prisma.blog.findFirst({
             where: {
-                id: body.id
+                id: Number(id)
             },
         })
 
         return c.json({
-            post 
+            blog 
         }) 
     }   catch(e){
             c.status(411)
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -10,6 +10,8 @@ export const userRouter = new Hono<{
     }
 }>();
 
+
+
 userRouter.post('/signup', async (c) => {
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
@@ -17,39 +19,60 @@ userRouter.post('/signup', async (c) => {
   
     const body = await c.req.json();
   
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
+    try {
+      const user = await prisma.user.create({
+        data: {
+          name : body.name,
+          username: body.username,
+          password: body.password
+        },
+      });
+
+      const token = await sign({ id: user.id }, c.env.JWT_SECRET)
+  
+      return c.json({
+        jwt: token
+      })
+    } catch(e){
+      console.log(e)
+      c.status(411)
+      return c.text("Invalid")
+    }
+    
+})
+  
+userRouter.post('/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: body.username,
+        password: body.password
       },
     });
-  
+
+    if(!user){
+      c.status(403)
+      return c.json({
+        message : "Incorrecr Credentils"
+      })
+    }
+
     const token = await sign({ id: user.id }, c.env.JWT_SECRET)
   
     return c.json({
       jwt: token
     })
-})
+  } catch(e){
+    console.log(e)
+    c.status(411)
+    return c.text("Invalid")
+  }
   
-userRouter.post('/signin', async (c) => {
-    const prisma = new PrismaClient({
-    //@ts-ignore
-        datasourceUrl: c.env?.DATABASE_URL	,
-    }).$extends(withAccelerate());
 
-    const body = await c.req.json();
-    const user = await prisma.user.findUnique({
-        where: {
-            email: body.email,
-            password: body.password
-        }
-    });
-
-    if (!user) {
-        c.status(403);
-        return c.json({ error: "user not found" });
-    }
-
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-    return c.json({ jwt });
 })
